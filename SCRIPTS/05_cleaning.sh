bb4df9777ab3a9ec6ebb5b2e38374224f8282732
KaneGreen <737445366KG@Gmail.com>
2020-05-27 14:50:38 +0800

    bug fix: cannot output hash

--- SCRIPTS/05_cleaning.sh
+++ SCRIPTS/05_cleaning.sh
@@ -2,6 +2,7 @@
 rm -rf `ls | grep -v "squashfs"`
 gzip -d *.gz
 gzip --best --keep *.img
-sha256sum * | tee hashes_${{ env.DATETIME }}.sha256
+sha256sum * | tee hashes_$(date "+%Y%m%d").sha256
+md5sum * | tee hashes_$(date "+%Y%m%d").md5
 rm -f *.img
 exit 0

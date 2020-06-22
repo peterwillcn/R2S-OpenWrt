5196ac70f8c45492b20e1af8ec663fdb42a01a6e
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-06-22 23:59:56 +0800

    clean up: 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -5,7 +5,5 @@ patch -p1 < ../PATCH/R2S.patch
 cd target/linux/rockchip
 mkdir patches-5.4
 cp -rf ../../../../PATCH/000-add-nanopi-r2s-support.patch patches-5.4/000-add-nanopi-r2s-support.patch
-cd ..
-cd ..
-cd ..
+cd ../../..
 exit 0
